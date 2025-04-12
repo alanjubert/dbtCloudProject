c0215ffdb88e337e66d630197e564e9240dcc96f
Alan Jubert dos Santos <alanjubert@gmail.com>
2025-04-12 04:14:14 +0000

    Build models on top of other models

--- models/customers.sql
+++ models/customers.sql
@@ -1,23 +1,32 @@
+{{
+  config(
+    materialized='view'
+  )
+}}
 with customers as (
 
-    select
+    /*select
         id as customer_id,
         first_name,
         last_name
 
-    from `dbt-tutorial`.jaffle_shop.customers
+    from `dbt-tutorial`.jaffle_shop.customers*/
+
+    select * from {{ ref("stg_customers") }}
 
 ),
 
 orders as (
 
-    select
+    /* select
         id as order_id,
         user_id as customer_id,
         order_date,
         status
 
     from `dbt-tutorial`.jaffle_shop.orders
+    */
+    select * from {{ ref("stg_orders") }}
 
 ),
 
